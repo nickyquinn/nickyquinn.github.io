033080184d1c93551a9dd17c5a3253b1e8db8bdc
Nick Quinn <nick@nickquinn.co.uk>
2024-03-23 22:44:03 +0000

    Use image optimiser from https://www.aleksandrhovhannisyan.com/blog/eleventy-image-plugin/#1-creating-a-stringifyattributes-utility
    Upgraded to Yarn 4

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,83 @@ const markdownItLinkAttr = require('markdown-it-link-attributes')
 const markdownItAnchor = require('markdown-it-anchor')
 const sass = require("sass");
 const path = require("node:path");
+const Image = require('@11ty/eleventy-img');
+const outdent = require('outdent');
+
+const imageShortcode = async (
+    src,
+    alt,
+    className = undefined,
+    widths = [400, 800, 1280],
+    formats = ['webp', 'jpeg'],
+    sizes = '100vw'
+) => {
+    console.log("SRC", src)
+    const imageMetadata = await Image(src, {
+        widths: [...widths, null],
+        formats: [...formats, null],
+        outputDir: 'docs/assets/images',
+        urlPath: '/assets/images',
+    });
+
+    const sourceHtmlString = Object.values(imageMetadata)
+        // Map each format to the source HTML markup
+        .map((images) => {
+            // The first entry is representative of all the others
+            // since they each have the same shape
+            const { sourceType } = images[0];
+
+            // Use our util from earlier to make our lives easier
+            const sourceAttributes = stringifyAttributes({
+                type: sourceType,
+                // srcset needs to be a comma-separated attribute
+                srcset: images.map((image) => image.srcset).join(', '),
+                sizes,
+            });
+
+            // Return one <source> per format
+            return `<source ${sourceAttributes}>`;
+        })
+        .join('\n');
+
+    const getLargestImage = (format) => {
+        const images = imageMetadata[format];
+        return images[images.length - 1];
+    }
+
+    const largestUnoptimizedImg = getLargestImage(formats[0]);
+    const imgAttributes = stringifyAttributes({
+        src: largestUnoptimizedImg.url,
+        width: largestUnoptimizedImg.width,
+        height: largestUnoptimizedImg.height,
+        alt,
+        loading: 'lazy',
+        decoding: 'async',
+    });
+    const imgHtmlString = `<img ${imgAttributes}>`;
+
+    const pictureAttributes = stringifyAttributes({
+        class: className,
+    });
+    const picture = `<picture ${pictureAttributes}>
+      ${sourceHtmlString}
+      ${imgHtmlString}
+    </picture>`;
+
+    return outdent`${picture}`;
+};
+
+/** Maps a config of attribute-value pairs to an HTML string
+ * representing those same attribute-value pairs.
+ */
+const stringifyAttributes = (attributeMap) => {
+    return Object.entries(attributeMap)
+        .map(([attribute, value]) => {
+            if (typeof value === 'undefined') return '';
+            return `${attribute}="${value}"`;
+        })
+        .join(' ');
+};
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addTemplateFormats("scss");
@@ -82,6 +159,8 @@ module.exports = function (eleventyConfig) {
             };
         }
     });
+
+    eleventyConfig.addShortcode('image', imageShortcode);
 
     return {
         dir: {
--- src/posts/including-project-dependencies-in-nuget-packages.md
+++ src/posts/including-project-dependencies-in-nuget-packages.md
@@ -14,20 +14,20 @@ But what if one of your published outputs is a NuGet package? Things work differ
 
 In the below screenshot I've set up a three-project solution: the first is the NuGet package project, the middle is a shared project the NuGet package project will have a dependency on, and the SampleNugetConsumer is what I'll attempt to install the resulting NuGet package from the first project into:
 
-![Set up of the example NuGet project](/assets/images/posts/nuget-project-dependencies/the_nuget_sample_project.webp)
+{% image "src/assets/images/posts/nuget-project-dependencies/the_nuget_sample_project.webp", "Set up of the example NuGet project" %}
 
 If I build this project, and look at the information for the NuGet package in the NuGet package manager, it tells me that this package has a dependency on SampleNuget.Shared:
 
-![NuGet package manager showing the package with NuGet dependencies](/assets/images/posts/nuget-project-dependencies/nuget_with_dependencies.webp)
+{% image "src/assets/images/posts/nuget-project-dependencies/nuget_with_dependencies.webp", "NuGet package manager showing the package with NuGet dependencies" %}
 
 Trying to install this NuGet package results in an error, because the dependent package is not available:
 
-![Missing dependency error](/assets/images/posts/nuget-project-dependencies/need_nuget_dependencies.webp)
+{% image "src/assets/images/posts/nuget-project-dependencies/need_nuget_dependencies.webp", "Missing dependency error" %}
 
 By making the following additions (in red) to the NuGet package's csproj file, the shared project will be built and the resulting DLL will be made part of the NuGet package, instead of an external dependency:
 
-![Missing dependency error](/assets/images/posts/nuget-project-dependencies/changes_to_nuget_project.webp)
+{% image "src/assets/images/posts/nuget-project-dependencies/changes_to_nuget_project.webp", "Missing dependency error" %}
 
 Now when I try to install the NuGet package, the external dependency is gone, and I can install the package, which now includes the shared project code:
 
-![Installable NuGet package with no external dependencies](/assets/images/posts/nuget-project-dependencies/dependency_free_nuget.webp)
+{% image "src/assets/images/posts/nuget-project-dependencies/dependency_free_nuget.webp", "Installable NuGet package with no external dependencies" %}
